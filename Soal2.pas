4d1191babe8907f168732f9baa121d9101979ffe
DhafaAfdhilla061 <dhafaafdhillah08@gmail.com>
2024-10-27 17:26:25 +0700

    Update Soal2.pas

--- Soal2.pas
+++ Soal2.pas
@@ -1,1 +1,24 @@
-//ketik kode jawaban no 2 kamu disini
+program sayur; //nama porgram
+uses crt;//memanggil unit crt
+var i,jp,js,j,hss,jss: integer;//deklarasi tipe data 
+th,ths,thd,tht,the,thl : real;//deklarasi tipe data
+begin//memulai program
+    clrscr;//membersihkan layar
+    write('Masukkan Jumlah Pelanggan : '); readln(jp);//input jumlah pelanggan
+    i:=1;//menginisialisasi nilai i 
+    while i<=jp do//operasi pengulangan
+    begin//memulai operasi pengulangan
+        writeln('Pelanggan ke-',i);
+        write('Masukkan Jumlah Jenis Sayur yang Dibeli : '); readln(js);
+        j:=1;
+        th:=0;
+            repeat
+                write('Masukkan harga sayur ke-',j,' : ');readln(hss);
+                write('Masukkan Jumlah Sayur ke-',j,' : ');readln(jss);
+                th:=th+(hss*jss); 
+                j:=j+1;
+            until j > js;
+        writeln('Total belanja untuk pelanggan ke-',i,': Rp',th:5:2);
+        i:=i+1;
+    end;
+end.
