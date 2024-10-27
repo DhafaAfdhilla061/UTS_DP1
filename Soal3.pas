a69f9d7154edfb95de6140e0aae46bfe32c82260
DhafaAfdhilla061 <dhafaafdhillah08@gmail.com>
2024-10-27 17:26:03 +0700

    Update Soal3.pas

--- Soal3.pas
+++ Soal3.pas
@@ -1,1 +1,20 @@
-//ketik kode jawaban noo 3 kamu disini
+program eksmen;//nama program 
+uses crt;//memanggil unit crt
+var n,i,j  : integer; //deklarasi tipe data n,i,j
+begin//memulai program
+    clrscr;//membersihkan layar
+    write('n = ');readln(n);//input nilai n 
+    if n mod 2 = 1 then //operasi kondisi n jika ganjil
+    begin//memulai operasi kondisi
+        for i:=1 to n do //operasi kondisi i(baris)
+        begin//memulai operasi kondisi i(baris)
+            for j := 1 to n do //operasi kondisi j(kolom)
+            begin//memulai operasi kondisi j(kolom)
+                if (i=j) or (i+j=n+1) then write('*')//operasi kondisi untuk output '*'
+                else write(' ');//operasi kondisi untuk output ' '
+            end;//mengakhiri operasi kondisi j(kolom)
+            writeln;//berpindah baris
+        end;//mengakhiri operasi 
+    end //mengakhiri operasi kondisi i(baris)
+    else write('angka tidak valid, harus angka ganjil');//operasi kondisi saat n genap
+end.//mengakhiri program 
